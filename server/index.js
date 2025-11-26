88829dbd292c6e1f200d760ce90107e0776cc434
Aisha-Ahmed-Al-Mashikhi <bentalakaber228@gmail.com>
2025-11-26 14:16:52 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,8 +25,11 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    #origin: "http://localhost:3000",
+    #methods: ["GET", "POST"],
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
+    credentials: true
   },
 });
 
@@ -34,8 +37,10 @@ const io = new Server(httpServer, {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    credentials: true,
+    #origin: "http://localhost:3000",
+    #credentials: true,
+    origin: process.env.CLIENT_URL,
+credentials: true
   })
 );
 
