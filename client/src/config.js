d76634c5c2c2fd549a4fb7a88fdbc5d401ab3bdd
Aisha-Ahmed-Al-Mashikhi <bentalakaber228@gmail.com>
2025-11-26 17:43:00 +0400

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+//export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+export const SERVER_URL = "https://utas-link-project-server.onrender.com";
