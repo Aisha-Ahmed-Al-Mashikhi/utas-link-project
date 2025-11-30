dabea105b79dbceb0163812ff14e98da30b4eb5c
Aisha-Ahmed-Al-Mashikhi <bentalakaber228@gmail.com>
2025-11-30 20:09:06 +0400

    Update Header.js

--- client/src/Component/Header.js
+++ client/src/Component/Header.js
@@ -88,7 +88,7 @@ const Header = () => {
                 <FiLogIn className="icon" /> Login
               </Link>
 
-              <Link to="/register" onClick={() => setOpen(false)}>
+              <Link to="/student-register" onClick={() => setOpen(false)}>
                 <FiUserPlus className="icon" /> Register
               </Link>
             </>
