ba0af276e7c54154e08fb278e4c9cd9f074269ff
Aisha-Ahmed-Al-Mashikhi <bentalakaber228@gmail.com>
2025-12-05 18:30:35 +0400

    Update PostModel.js

--- server/Models/PostModel.js
+++ server/Models/PostModel.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema(
   {
-    name: { type: String, required: false },      // ✔ يظهر اسم المستخدم
-    email: { type: String, required: true },
-    role: { type: String, default: "student" },   // ✔ لو طالب أو شركة
-
     postMsg: { type: String, required: true },
+  email: { type: String, required: true },
+  authorName: { type: String, required: true }, // ⭐ جديد
+  createdAt: { type: Date, default: Date.now },
 
     // LOCATION
     location: {
