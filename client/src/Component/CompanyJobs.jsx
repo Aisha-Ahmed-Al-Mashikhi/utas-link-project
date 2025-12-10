d1567661e663c94b1a39dc9905f8dafe4656bb7f
Aisha-Ahmed-Al-Mashikhi <bentalakaber228@gmail.com>
2025-12-10 19:04:09 +0400

    Update CompanyJobs.jsx

--- client/src/Component/CompanyJobs.jsx
+++ client/src/Component/CompanyJobs.jsx
@@ -123,7 +123,7 @@ const CompanyJobs = () => {
                 {/* NEW — VIEW APPLICANTS */}
                 <button
                   className="applicants-btn"
-                  onClick={() => navigate(`/company/applicants?jobId=${job._id}`) }
+                  onClick={() => navigate(`/company/applicants?jobId=${viewJob._id}`)}
                 >
                   Applicants
                 </button>
