f40fc335f96aacc3fb91ad92ea60ddb8a8de0dba
Aisha-Ahmed-Al-Mashikhi <bentalakaber228@gmail.com>
2025-12-12 16:51:29 +0400

    Update store.js

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -1,4 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+/* ================= REDUX PERSIST ================= */
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage
+import { combineReducers } from "redux";
+
+/* ================= REDUCERS ================= */
 import usersReducer from "../Features/UserSlice";
 import companyReducer from "../Features/CompanySlice";
 import jobReducer from "../Features/JobSlice";
@@ -6,15 +13,33 @@ import applicationReducer from "../Features/ApplicationSlice";
 import chatReducer from "../Features/ChatSlice";
 import postReducer from "../Features/PostSlice";
 
-const store = configureStore({
-  reducer: {
-    applications: applicationReducer,
-    users: usersReducer,
-    companies: companyReducer,
-    jobs: jobReducer,
-    chat: chatReducer,
-    posts: postReducer,
-  },
+/* ================= PERSIST CONFIG ================= */
+const persistConfig = {
+  key: "reduxstore",
+  storage,
+};
+
+/* ================= ROOT REDUCER ================= */
+const rootReducer = combineReducers({
+  applications: applicationReducer,
+  users: usersReducer,
+  companies: companyReducer,
+  jobs: jobReducer,
+  chat: chatReducer,
+  posts: postReducer,
 });
 
+/* ================= PERSISTED REDUCER ================= */
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+/* ================= STORE ================= */
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+/* ================= PERSISTOR ================= */
+const persistore = persistStore(store);
+
+/* ================= EXPORTS ================= */
+export { store, persistore };
 export default store;
