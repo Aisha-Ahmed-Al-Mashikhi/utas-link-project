707e5ea07bcb19e6682b6cd9bd9da78c0daa2cc7
Aisha-Ahmed-Al-Mashikhi <bentalakaber228@gmail.com>
2025-12-14 22:47:19 +0400

    Update ApplicantsJob.jsx

--- client/src/Component/ApplicantsJob.jsx
+++ client/src/Component/ApplicantsJob.jsx
@@ -89,7 +89,7 @@ const ApplicantsJob = () => {
                       rel="noopener noreferrer"
                       className="cv-btn"
                     >
-                      View CV
+                      CV
                     </a>
                   )}
 
